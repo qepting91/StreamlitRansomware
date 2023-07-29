fff26a9352d183e9b7320d18eec43ef4dfd06154
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-07-29 10:29:40 -0400

    Update rss_reader.py

--- rss_reader.py
+++ rss_reader.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from data_load import load_rss_data
+from data_process import process_data
 
 def rss_reader():
     rss_data = load_rss_data()
+    rss_data = process_data(rss_data)  # Apply the same processing used for the main data
     st.title("RSS Reader")
 
     for index, row in rss_data.iterrows():
