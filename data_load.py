2996e09460dca2db436f2b4aaee7acda9247e4fe
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-07-29 09:54:54 -0400

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -6,17 +6,18 @@ def convert_sheets_to_csv_url(sheets_url):
     return sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
 
 # Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
 def load_data():
     url = convert_sheets_to_csv_url("https://docs.google.com/spreadsheets/d/1GxUiFb00sCTytJ4XkegN8qnZ4WAmsXWGVV98eMN8F_I/edit#gid=2084622647")
-    data = pd.read_csv(url, usecols=[0, 1, 2])
+    data = pd.read_csv(url, parse_dates=['Date'], date_parser=lambda x: pd.to_datetime(x, format='%b %d, %Y'))
     # Return a copy of the data to allow for mutation without affecting the cached data
     return data.copy()
 
-@st.cache_data(ttl=600)
+@st.cache(ttl=600)
 def load_rss_data():
     url = convert_sheets_to_csv_url("https://docs.google.com/spreadsheets/d/1GxUiFb00sCTytJ4XkegN8qnZ4WAmsXWGVV98eMN8F_I/edit#gid=1687625614")
-    rss_data = pd.read_csv(url, usecols=[0, 1, 2])
+    rss_data = pd.read_csv(url, parse_dates=['Date'], date_parser=lambda x: pd.to_datetime(x, format='%b %d, %Y'))
     # Return a copy of the data to allow for mutation without affecting the cached data
     return rss_data.copy()
+
