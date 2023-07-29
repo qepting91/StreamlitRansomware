64c31873b047e2719af9aa95aea6bfbba41f2bca
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-07-29 10:05:50 -0400

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -11,13 +11,13 @@ def convert_sheets_to_csv_url(sheets_url):
 def load_data():
     url = convert_sheets_to_csv_url("https://docs.google.com/spreadsheets/d/1GxUiFb00sCTytJ4XkegN8qnZ4WAmsXWGVV98eMN8F_I/edit#gid=2084622647")
     data = pd.read_csv(url, parse_dates=['Date'], date_parser=lambda x: pd.to_datetime(x, format='%b %d, %Y'))
-     data['Date'] = data['Date'].dt.strftime('%b %Y')
+    data['Date'] = data['Date'].dt.strftime('%b %Y')
     return data.copy()
 
 @st.cache_data(ttl=600)
 def load_rss_data():
     url = convert_sheets_to_csv_url("https://docs.google.com/spreadsheets/d/1GxUiFb00sCTytJ4XkegN8qnZ4WAmsXWGVV98eMN8F_I/edit#gid=1687625614")
     rss_data = pd.read_csv(url, parse_dates=['Date'], date_parser=lambda x: pd.to_datetime(x, format='%b %d, %Y'))
-    # Return a copy of the data to allow for mutation without affecting the cached data
+    data['Date'] = data['Date'].dt.strftime('%b %Y')
     return rss_data.copy()
 
