94d4ae24f2fa1ed50ca0741af3d5437339cd53ac
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-08-02 08:10:21 -0400

    Update data_load.py
    updated load data to cockroachdb

--- data_load.py
+++ data_load.py
@@ -1,18 +1,34 @@
 import pandas as pd
 import streamlit as st
+from sqlalchemy import create_engine, Table, MetaData, select
 
 # Function to convert Google Sheets URL to CSV URL
 def convert_sheets_to_csv_url(sheets_url):
     return sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
 
-# Read in data from the Google Sheet.
+# Read in data from CockroachDB.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+@st.cache(ttl=600)
 def load_data():
-    url = convert_sheets_to_csv_url("https://docs.google.com/spreadsheets/d/1GxUiFb00sCTytJ4XkegN8qnZ4WAmsXWGVV98eMN8F_I/edit#gid=2084622647")
-    data = pd.read_csv(url, parse_dates=['Date'], date_parser=lambda x: pd.to_datetime(x, format='%b %d, %Y'))
-    data['Date'] = data['Date'].dt.strftime('%b %Y')
+    # Setup the database connection
+    database_url = st.secrets["database"]["url"]
+    engine = create_engine(database_url)
+
+    # Reflect the ransomware_fulldata table
+    metadata = MetaData()
+    ransomware_fulldata = Table('ransomware_fulldata', metadata, autoload_with=engine)
+
+    # Query the table
+    stmt = select(ransomware_fulldata)
+    with engine.connect() as connection:
+        result = connection.execute(stmt)
+        data = pd.DataFrame(result.fetchall(), columns=result.keys())
+
+    # Convert the 'date' column to datetime format and reformat it to '%b %Y'
+    data['date'] = pd.to_datetime(data['date']).dt.strftime('%b %Y')
+
     return data.copy()
+
 
 @st.cache_data(ttl=600)
 def load_rss_data():
