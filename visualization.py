c76778625192b67369df2525aaf2bd5caddbf414
qepting91 <64446420+qepting91@users.noreply.github.com>
2023-08-02 14:26:06 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import base64
 import plotly.graph_objects as go
+from data_process import process_data
 
 def display_options(data):
+    data = process_data(data)
     pd.set_option('display.max_colwidth', None)
 
     st.sidebar.markdown('<p class="font">Ransomware Data Database</p>', unsafe_allow_html=True)
@@ -14,7 +16,7 @@ def display_options(data):
 
     st.sidebar.subheader('Statistics')
     if st.sidebar.checkbox('Full Data Set', key='full_data_set'):
-        st.subheader('Data Starting with Most Recent Attacks Filter on Entry Number at the Left of the Table')
+        st.subheader('Filter on Entry Number at the Left of the Table for Most Recent Attack')
         st.write(data[['group', 'title', 'date']].sort_values(by='date', ascending=False))
 
     if st.sidebar.checkbox('Show attacks over time', help='Toggle to view the number of attacks over time'):
@@ -29,22 +31,21 @@ def display_options(data):
         else:
             # Convert 'date' column back to datetime type for resampling
             filtered_data['date'] = pd.to_datetime(filtered_data['date'])
-        
+
             # Set 'date' as index and resample the data monthly
             filtered_data = filtered_data.set_index('date').groupby('group').resample('M').size().reset_index(name='Number of Attacks')
-            filtered_data['date'] = filtered_data['date'].dt.strftime('%b %d, %Y')
-        
+
             # Initialize an empty figure
             fig = go.Figure()
-        
+
             # Define colors for the groups
             colors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52']
-        
+
             # Loop over the groups to add a line for each group
             for i, group in enumerate(selected_groups):
                 group_data = filtered_data[filtered_data['group'] == group]
                 fig.add_trace(go.Scatter(
-                    x=group_data['date'], 
+                    x=group_data['date'].dt.strftime('%b %d, %Y'),  # Format date when plotting
                     y=group_data['Number of Attacks'], 
                     mode='lines+markers+text', 
                     name=group, 
@@ -75,7 +76,7 @@ def display_options(data):
                 st.markdown("""
                     The chart above depicts the number of ransomware attacks over time for the selected groups. 
                     The x-axis represents time (in months and years), while the y-axis represents the count of attacks. Each line corresponds to a different group.
-                    The markers show the number of attacks for each month. The plot allows us to analyze the frequency and trend of attacks over time for each group.
+                    The markers show the number of attacks for each day of the month. The plot allows us to analyze the frequency and trend of attacks over time for each group.
                 """)
 
     if st.sidebar.checkbox('Download CSV file'):
